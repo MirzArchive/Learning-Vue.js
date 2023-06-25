5a076aed8a97cd44f4807a3b861c2002334c5a5c
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-06-25 19:40:22 +0700

    `feat:` Complete Chapter 16: Working with Forms

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
-  <navbar :pages="pages" :active-page="activePage" :nav-page-click="(key) => activePage = key"></navbar>
-  <page-viewer :page="pages[activePage]" v-if="pages != null"></page-viewer>
+  <Navbar
+    :pages="pages"
+    :active-page="activePage"
+    :nav-page-click="(key) => activePage = key"
+  ></Navbar>
+  <PageViewer
+    :page="pages[activePage]"
+    v-if="pages != null"
+  ></PageViewer>
+
+  <CreatePage :page-created="createPage"></CreatePage>
 </template>
 
 <script>
 import Navbar from './components/NavBar.vue';
 import PageViewer from './components/PageViewer.vue';
+import CreatePage from './components/CreatePage.vue';
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 
 export default {
   components: {
     Navbar,
-    PageViewer
+    PageViewer,
+    CreatePage
   },
   created() {
     this.setPages()
@@ -28,6 +39,9 @@ export default {
       let data = await res.json()
 
       this.pages = data
+    },
+    createPage(pageObj) {
+      console.log(pageObj);
     }
   }
 }
---
+++ src/components/CreatePage.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="container mb-3">
+		<form action="">
+			<div class="mb-3">
+				<div class="form-label">Page Title</div>
+				<input
+					type="text"
+					name=""
+					id=""
+					class="form-control"
+					v-model="pageTitle"
+				>
+			</div>
+			<div class="mb-3">
+				<label
+					for=""
+					class="form-label"
+				>Content</label>
+				<textarea
+					type="text"
+					class="form-control"
+					name=""
+					id=""
+					cols="30"
+					rows="10"
+					v-model="pageContent"
+				></textarea>
+			</div>
+			<div class="mb-3">
+				<button
+					class="btn btn-primary"
+					@click.prevent="pageCreated({pageTitle, pageContent})"
+				>Create Page</button>
+			</div>
+		</form>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: ['pageCreated'],
+		data() {
+			return {
+				pageTitle: '',
+				pageContent: ''
+			}
+		}		
+	}
+</script>
+
+<style scoped></style>
